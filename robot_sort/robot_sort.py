11491faaa7cea362c1573c1c1db84d9f3c87ff25
Julie Thrall <juliethrallstewart@gmail.com>
2019-12-13 11:14:19 -0800

    implementing sorting on robot, still needs cache

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -92,12 +92,23 @@ class SortingRobot:
         """
         return self._light == "ON"
 
-    def sort(self):
+    def sort(self, l):
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        #next step = implement cache due to max calls 
+        cache = {}
+        for i in range(0, len(l) - 1):
+            cur_index = i
+            if l[cur_index] <= l[cur_index + 1]:
+                print(f"if {l[cur_index]} <= {l[cur_index + 1]} do nothing")
+    
+            elif l[cur_index] > l[cur_index + 1]:
+                print(f"else {l[cur_index]} > {l[cur_index + 1]} swap variables")
+                l[cur_index], l[cur_index + 1] = l[cur_index + 1], l[cur_index]
+
+                return self.sort(l)
+        return l
 
 
 if __name__ == "__main__":
@@ -105,8 +116,8 @@ if __name__ == "__main__":
     # with `python robot_sort.py`
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    lst = [1,87,45,6,78,9,4,4,14,15,16,18,25,23,22,24]
+    robot = SortingRobot(lst)
 
-    robot = SortingRobot(l)
-
-    robot.sort()
+    robot.sort(lst)
     print(robot._list)
